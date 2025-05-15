73072a96d6864c78b7f98be34a112bed6b9edaf0
Pr4veenn <praveen.tech.js@gmail.com>
2025-05-15 15:54:55 +0530

    useEfect, shimeer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,31 @@
 import Restaurantcard from "./Restaurantcard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 
 const Body = () => {
 
     // useState - Hooks
-    const [ListOfRestaurants, setListOfRestaurant] = useState(resList)
+    const [ListOfRestaurants, setListOfRestaurant] = useState([])
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    // api call to fetch data
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/search/suggest?lat=9.9312328&lng=76.26730409999999&str=resta&trackingId=undefined&includeIMItem=true")
+
+        const json = await data.json()
+        console.log(json)
+        setListOfRestaurant(json.data.suggestions)
+    }
+
+    // add shimmer ui while page loading
+    if(ListOfRestaurants.length === 0) {
+        return <Shimmer />
+    }
 
     return (
         <div className="body">
@@ -17,8 +36,8 @@ const Body = () => {
                 }}>Top Rated Restaurants</button>
             </div>
             <div className="restro-container">
-                {ListOfRestaurants.map((restaurant) => (
-                    <Restaurantcard key={restaurant.id} resData={restaurant}/>
+                {ListOfRestaurants.map((restaurant, index) => (
+                    <Restaurantcard key={index} resData={restaurant}/>
                 ))}
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import { LOGO_URL } from "../utils/constants";
+import {useState} from "react"
 
 const Header = () => {
+    const [LoginBtn, setLogoutBtn] = useState("Login")
+    console.log("login button click")
     return (
         <div className="header">
             <div className="logo-container">
@@ -12,6 +15,12 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="login" onClick={() => {
+                        LoginBtn === "Login" ? setLogoutBtn("Logout") : setLogoutBtn("Login")
+                    }}
+                    >
+                        {LoginBtn}
+                    </button>
                 </ul>
             </div>
         </div>
